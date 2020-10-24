ed956544a68107d1edfb22e3e75e109542c25473
João Moraes <joaovitordmoraes@gmail.com>
2020-10-24 12:01:43 -0300

    :sparkles: Create project gallery logic

---
+++ assets/js/gallery.js
@@ -0,0 +1,46 @@
+(function () { 
+
+    fetch('https://picsum.photos/v2/list')
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        getImage(data);
+        
+    })
+
+    function getImage(data) {
+        document.querySelector('.projects-list').insertAdjacentHTML('afterbegin', '<div class="project-carousel"></div>')
+        data.forEach(image => {
+            const $item = document.querySelector('.project-carousel');
+            $item.insertAdjacentHTML('afterbegin', `<div class="carousel-cell"><img src="${image.download_url}" alt=""></div>`);
+        });
+
+        setTimeout(() => {
+            const $projectOptions = {
+                cellAlign: 'left',
+                wrapAround: true,
+                groupCells: '100%',
+                watchCSS: true,
+                pageDots: false,
+                prevNextButtons: false
+            }
+
+            const $carousel = new Flickity('.project-carousel', $projectOptions);
+    
+            let previousButton = document.querySelector('.arrow.-prev');
+        
+            previousButton.addEventListener( 'click', function() {
+                $carousel.previous();
+            });
+        
+            let nextButton = document.querySelector('.arrow.-next');
+        
+            nextButton.addEventListener( 'click', function() {
+                $carousel.next();
+            });
+        }, 700);
+        
+    }
+
+})();
