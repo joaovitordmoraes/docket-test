99248251ae03957a745d33fda77c178e7f8f660c
João Moraes <joaovitordmoraes@gmail.com>
2020-10-25 19:56:49 -0300

    :sparkles: Finish form logic

--- assets/js/form.js
+++ assets/js/form.js
@@ -5,28 +5,49 @@
     const email = document.querySelector('.apply-form .email');
     const tel1 = document.querySelector('.apply-form .tel1');
     const tel2 = document.querySelector('.apply-form .tel2');
-
+    const success = document.querySelector('.success');
+    const inputs = document.querySelectorAll('input');
+    const applyContent = document.querySelector('.apply-content');
+    
     submit.addEventListener('click', function(event) {
         event.preventDefault();
 
-        if (!name.checkValidity()) {
-            // name.after(`<span class="error">${name.validationMessage}</span>`)
-            name.nextElementSibling.innerHTML = 'Digite um nome válido'
-            // document.querySelector(".error").innerHTML = name.validationMessage;
-        }
+        validate();
 
-        if (name.checkValidity()) {
+        if (name.checkValidity() && email.checkValidity() && tel1.checkValidity() && tel2IsValid) {
+            success.classList.add('-active');
+            submit.setAttribute('disabled', true);
+
+            setTimeout(() => {
+                success.classList.remove('-active');
+                success.classList.add('-inactive');
+
+                inputs.forEach(input => {
+                    input.value = '';
+                });
+
+                submit.removeAttribute('disabled');
+            }, 2000);
+
             descriptionText.classList.add('-hidden');
-            sendData();
+
+            if(!applyContent.classList.contains('-active')) {
+                sendData();
+            } else {
+                const infoContent = document.querySelector('.info-content');
+                
+                success.classList.remove('-inactive');
+                applyContent.classList.remove('-active');
+                infoContent.remove();
+
+                sendData();
+            }
+
         }
     });
-
-
     
-    
-
     function sendData() {
-        const content = document.querySelector('.heading')
+        const content = document.querySelector('.apply-content')
             ,createInfo = document.createElement('div')
 
         createInfo.classList.add('info-content')
@@ -48,10 +69,56 @@
 
         <div class="field">
             <p class="name">Telefone 2:</p>
-            <p class="content -empty">${tel2.value}</p>
+            <p class="content ${tel2.value > '' ? tel2.value : '-empty'}">${tel2.value > '' ? tel2.value : 'Não informado'}</p>
         </div>
         `
+
+        applyContent.classList.add('-active');
         
         content.insertAdjacentElement('beforeend', createInfo)
     }
+
+    function validate() {
+        if (!name.checkValidity()) {
+            name.nextElementSibling.innerHTML = 'Digite um nome válido'
+        } else {
+            name.nextElementSibling.innerHTML = ''
+        }
+
+        if (!email.checkValidity()) {
+            email.nextElementSibling.innerHTML = 'Digite um e-mail válido'
+        } else {
+            email.nextElementSibling.innerHTML = ''
+        }
+
+        if (!tel1.checkValidity()) {
+            tel1.nextElementSibling.innerHTML = 'Digite um telefone válido'
+        } else {
+            tel1.nextElementSibling.innerHTML = ''
+        }
+
+        if (tel2.value.length < 14 && tel2.value != '') {
+            tel2.nextElementSibling.innerHTML = 'Digite um telefone válido'
+        } else {
+            tel2.nextElementSibling.innerHTML = ''
+        }
+
+        return tel2IsValid = tel2.value.length >= 14 && tel2.value != '' || tel2.value.length < 14 && tel2.value == '';
+    }
+
+    function inputHandler(masks, max, event) {
+        var c = event.target;
+        var v = c.value.replace(/\D/g, '');
+        var m = c.value.length > max ? 1 : 0;
+        VMasker(c).unMask();
+        VMasker(c).maskPattern(masks[m]);
+        c.value = VMasker.toPattern(v, masks[m]);
+    }
+    
+    var telMask = ['(99) 9999-99999', '(99) 99999-9999'];
+    var tel = document.querySelectorAll('.phone');
+    VMasker(tel).maskPattern(telMask[0]);
+    tel.forEach(phone => {
+        phone.addEventListener('input', inputHandler.bind(undefined, telMask, 14), false);
+    });
 })();
